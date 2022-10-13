a1f2ab007e88bb6ce7416c3e488977d40d93d692
rumpear <roman.f.grusha@gmail.com>
2022-10-13 13:00:09 +0300

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,10 @@ import './index.css';
 // import '../src/data/practiceEnum';
 // import '../src/data/enum';
 // import '../src/data/additionalTypes';
-import './data/classes/overload';
-import './data/classes/methods';
+// import './data/classes/overload';
+// import './data/classes/methods';
+// import './data/classes/getSet';
+// import './data/classes/extends';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
