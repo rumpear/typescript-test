876d85fd98db5b177e6828c0621a4a9dffe4551d
Roman <77742141+rumpear@users.noreply.github.com>
2022-10-14 23:28:24 +0300

    CartView added func

--- src/components/OnlineShop/CartView/CartView.tsx
+++ src/components/OnlineShop/CartView/CartView.tsx
@@ -1,5 +1,46 @@
-const CartView = () => {
-  return <></>;
+import { useState, useEffect } from 'react';
+import { IProductsData } from '../types';
+
+interface IProps {
+  removeFromCart: (id: number) => void;
+  products: IProductsData[];
+}
+
+const CartView = ({ products, removeFromCart }: IProps) => {
+  console.log(products, 'products');
+  const [productsData, setProductsData] = useState<IProductsData[]>(products);
+  console.log(productsData, 'CartView productsData');
+
+  useEffect(() => {
+    setProductsData(products);
+  }, [products]);
+
+  return (
+    <>
+      {productsData.length ? (
+        <>
+          <h1>In cart</h1>
+          {productsData.map(({ id, name, price }) => {
+            return (
+              <div key={id}>
+                <h5>{name}</h5>
+                <h6>{price}</h6>
+                {/* <button type="button" onClick={() => handleAddProduct(id)}>
+                  Add to cart
+                </button> */}
+
+                {/* <button type="button" onClick={handleAddProduct}>
+                Remove from cart
+              </button> */}
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <h1>Nothing in cart</h1>
+      )}
+    </>
+  );
 };
 
 export default CartView;
