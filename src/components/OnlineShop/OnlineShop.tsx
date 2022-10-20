0238e9fb8fc662a7fe1b0a7e80fd9cb860f53e8c
rumpear <roman.f.grusha@gmail.com>
2022-10-20 11:58:15 +0300

    OnlineShop refactoring

--- src/components/OnlineShop/OnlineShop.tsx
+++ src/components/OnlineShop/OnlineShop.tsx
@@ -1,144 +1,210 @@
 import { Component } from 'react';
 import { CartView } from './CartView';
 import { ProductsList } from './ProductsList';
-import { TShopId, IProductsData, IDelivery } from './types';
+import { Delivery } from './Delivery';
+import { SuccessMessage } from './SuccessMessage';
+import { DELIVERY_OPTIONS, PRODUCTS_DATA, SHOP_LIST } from './data';
+import { IProductsData, IShopId } from './types';
+import s from './OnlineShop.module.css';
 
-const PRODUCTS_DATA = [
-  { id: 1, name: 'keyboard', price: 80 },
-  { id: 2, name: 'mouse', price: 120 },
-  { id: 3, name: 'gamepad', price: 80 },
-  { id: 4, name: 'RAM', price: 100 },
-  { id: 5, name: 'display', price: 700 },
-  { id: 6, name: 'motherboard', price: 200 },
-  { id: 7, name: 'GPU', price: 1000 },
-];
-
-class Delivery implements IDelivery {
-  date?: Date | string;
-  userAddress?: string;
-  shopId?: TShopId;
-
-  homeDelivery(date: string, userAddress: string) {
-    this.date = date;
-    this.userAddress = userAddress;
-  }
-
-  selfDelivery(shopId: TShopId) {
-    this.date = new Date();
-    this.shopId = shopId;
-  }
+interface IStateOnlineShop {
+  productsData: IProductsData[];
+  date: string;
+  userAddress: string;
+  shopId: IShopId | null;
+  totalPrice: number;
+  isDeliveryReady: boolean;
+  isOrderReady: boolean;
 }
 
-// type TAddToCart = (id: number) => void;
+class OnlineShop extends Component<{}, IStateOnlineShop> {
+  state: IStateOnlineShop = {
+    productsData: [],
+    date: '',
+    userAddress: '',
+    shopId: null,
+    totalPrice: 0,
+    isDeliveryReady: false,
+    isOrderReady: false,
+  };
 
-class Cart extends Delivery {
-  products!: IProductsData[];
-  totalPrice?: number = 0;
-
-  constructor(products?: IProductsData[]) {
-    super();
-    this.products = products ?? [];
-  }
-
-  addToCart(id: number) {
-    const item = PRODUCTS_DATA.find(p => id === p.id);
-
-    if (item) {
-      this.products.push(item);
+  componentDidUpdate(_: never, prevState: IStateOnlineShop) {
+    const totalPrice = this.countTotalPrice();
+    if (prevState.totalPrice !== totalPrice) {
+      this.setState({ totalPrice });
     }
   }
 
-  removeFromCart(id: number) {
-    this.products = this.products.filter(p => p.id !== id);
-  }
-
-  countTotalPrice() {
-    this.totalPrice = this.products.reduce((prev, product) => {
-      return prev + product.price;
-    }, 0);
-  }
-
-  checkoutCartConditions() {
-    const isDeliverySelected = this.userAddress || this.shopId;
-    return Boolean(isDeliverySelected && this.products);
-  }
-}
-
-interface IProps {
-  initialValue: number;
-}
-
-interface IState {
-  productsData: IProductsData[];
-  date: Date | string;
-  userAddress: string;
-  shopId: TShopId | null;
-}
-
-class OnlineShop extends Component {
-  // <IProps, IState>
-  state: IState = {
-    productsData: [],
-    date: new Date(),
-    userAddress: '',
-    shopId: null,
-  };
-
   addToCart = (id: number): void => {
-    const product = PRODUCTS_DATA.find((p: IProductsData) => id === p.id);
+    const initProduct = this.state.productsData.find(
+      (product: IProductsData) => id === product.id,
+    );
 
-    this.setState(prev => {
-      return { ...prev, productsData: [...this.state.productsData, product] };
+    if (!initProduct) {
+      const newProduct = PRODUCTS_DATA.find(
+        (product: IProductsData) => id === product.id,
+      );
+
+      if (newProduct) {
+        return this.setState({
+          productsData: [
+            ...this.state.productsData,
+            { ...newProduct, quantity: 1 },
+          ],
+        });
+      }
+    }
+
+    const productList = this.state.productsData.map(product => {
+      const isProductSelected = product.id === id;
+      const updatedProduct = {
+        ...product,
+        quantity: product.quantity + 1,
+      };
+
+      return isProductSelected ? updatedProduct : product;
     });
 
-    // const { setState, state } = this;
-    // const product = PRODUCTS_DATA.find((p: IProductsData) => id === p.id);
-    // setState(prev => {
-    //   return { ...prev, productsData: [...state.productsData, product] };
-    // });
+    this.setState({ productsData: productList });
   };
 
-  removeFromCart(id: number) {
-    // const { productsData } = this.state;
-    // const { state } = this;
+  removeFromCart = (id: number): void => {
     const filteredData = this.state.productsData.filter(
-      (p: IProductsData) => p.id !== id,
+      (product: IProductsData) => product.id !== id,
     );
-    // this.setState(prev => {
-    //   return prev.this.state, {};
-    //   // [...prev.state, ...filteredData];
-    // });
-    // this.setState(prev => {
-    //   // console.log(prev);
-    //   return { ...prev, ...filteredData };
-    // });
+
     this.setState({ productsData: filteredData });
-  }
+  };
 
-  // countTotalPrice() {
-  //   this.totalPrice = this.products.reduce((prev, product: IProductsData) => {
-  //     return prev + product.price;
-  //   }, 0);
-  // }
+  countTotalPrice = (): number => {
+    const totalPrice = this.state.productsData.reduce(
+      (prev: number, product: IProductsData) => {
+        return product.price * product.quantity + prev;
+      },
+      0,
+    );
 
-  // checkoutCartConditions() {
-  //   const isDeliverySelected = this.userAddress || this.shopId;
-  //   return Boolean(isDeliverySelected && this.productsData);
-  // }
+    return totalPrice;
+  };
+
+  incrementQuantity = (id: number): void => {
+    const incrementedProducts = this.state.productsData.map(
+      (product: IProductsData) => {
+        const isIdMatch = product.id === id;
+
+        return isIdMatch
+          ? { ...product, quantity: product.quantity + 1 }
+          : product;
+      },
+    );
+
+    this.setState({ productsData: incrementedProducts });
+  };
+
+  decrementQuantity = (id: number): void => {
+    const decrementedProducts = this.state.productsData.map(
+      (product: IProductsData) => {
+        const isIdMatch = product.id === id;
+        const isQuantityGreaterThanZero = product.quantity > 0;
+
+        return isIdMatch && isQuantityGreaterThanZero
+          ? { ...product, quantity: product.quantity - 1 }
+          : product;
+      },
+    );
+
+    const filteredProducts = decrementedProducts.filter(
+      (products: IProductsData) => products.quantity > 0,
+    );
+    this.setState({ productsData: filteredProducts });
+  };
+
+  setDeliveryStatus = (status: boolean): void => {
+    this.setState({ isDeliveryReady: status });
+  };
+
+  setOrderStatus = (status: boolean): void => {
+    this.setState({ isOrderReady: status });
+  };
+
+  checkoutCartConditions = (): boolean => {
+    const isDeliverySelected = this.state.userAddress || this.state.shopId;
+    const isProductsDataExist = this.state.productsData.length;
+    return Boolean(isDeliverySelected && isProductsDataExist);
+  };
+
+  makeOrder = (value: string | IShopId): void => {
+    const date = new Date().toISOString();
+
+    this.setState({ date });
+
+    if (typeof value === 'string') {
+      return this.setState({ userAddress: value, shopId: null });
+    }
+
+    this.setState({ shopId: value, userAddress: '' });
+  };
 
   render() {
-    const { addToCart, removeFromCart } = this;
-    const { productsData } = this.state;
-    console.log(productsData, 'productsData OnlineShop');
+    const {
+      addToCart,
+      removeFromCart,
+      incrementQuantity,
+      decrementQuantity,
+      setDeliveryStatus,
+      setOrderStatus,
+      makeOrder,
+      checkoutCartConditions,
+    } = this;
+
+    const {
+      productsData,
+      totalPrice,
+      isDeliveryReady,
+      isOrderReady,
+      userAddress,
+      date,
+      shopId,
+    } = this.state;
+
+    const isCartCheckedSuccessfully = checkoutCartConditions();
 
     return (
       <>
-        <ProductsList products={PRODUCTS_DATA} addToCart={addToCart} />
-        <CartView products={productsData} removeFromCart={removeFromCart} />
+        <div className={s.wrapper}>
+          <ProductsList products={PRODUCTS_DATA} addToCart={addToCart} />
+        </div>
+
+        <CartView
+          products={productsData}
+          totalPrice={totalPrice}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          removeFromCart={removeFromCart}
+          setDeliveryStatus={setDeliveryStatus}
+          isDeliveryReady={isDeliveryReady}
+        />
+
+        {isDeliveryReady && (
+          <Delivery
+            shopList={SHOP_LIST}
+            deliveryOptions={DELIVERY_OPTIONS}
+            makeOrder={makeOrder}
+            setOrderStatus={setOrderStatus}
+          />
+        )}
+
+        {isOrderReady && isCartCheckedSuccessfully && (
+          <SuccessMessage
+            userAddress={userAddress}
+            shopAddress={shopId?.address}
+            totalPrice={totalPrice}
+            date={date}
+          />
+        )}
       </>
     );
   }
 }
-// const foo = new OnlineShop({});
-// foo.removeFromCart(1);
+
 export default OnlineShop;
