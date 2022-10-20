82b0291f7c830d2582cde5823e6aa197d19ef09f
rumpear <roman.f.grusha@gmail.com>
2022-10-20 11:57:56 +0300

    OnlineShop data added

---
+++ src/components/OnlineShop/data.ts
@@ -0,0 +1,23 @@
+export const PRODUCTS_DATA = [
+  { id: 1, name: 'keyboard', price: 80, quantity: 0 },
+  { id: 2, name: 'mouse', price: 120, quantity: 0 },
+  { id: 3, name: 'gamepad', price: 80, quantity: 0 },
+  { id: 4, name: 'RAM', price: 100, quantity: 0 },
+  { id: 5, name: 'display', price: 700, quantity: 0 },
+  { id: 6, name: 'motherboard', price: 200, quantity: 0 },
+  { id: 7, name: 'GPU', price: 1000, quantity: 0 },
+];
+
+export const SHOP_LIST = [
+  { id: 0, address: 'Odesa' },
+  { id: 1, address: 'Kyiv' },
+  { id: 2, address: 'Cherkasy' },
+  { id: 3, address: 'Kharkiv' },
+  { id: 4, address: 'Lviv' },
+  { id: 5, address: 'Dnipro' },
+];
+
+export const DELIVERY_OPTIONS = [
+  { id: 0, label: 'home' },
+  { id: 1, label: 'self' },
+];
