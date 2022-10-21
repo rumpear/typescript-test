38b06337969ccff9b9273522d16ffc54fef37da3
rumpear <roman.f.grusha@gmail.com>
2022-10-21 18:56:10 +0300

    CartView upd

--- src/components/OnlineShop/CartView/CartView.tsx
+++ src/components/OnlineShop/CartView/CartView.tsx
@@ -1,43 +1,68 @@
-import { useState, useEffect } from 'react';
+import { Controls } from '../../Counter/Controls';
 import { IProductsData } from '../types';
+import s from './CartView.module.css';
 
 interface IProps {
-  removeFromCart: (id: number) => void;
   products: IProductsData[];
+  totalPrice: number;
+  isDeliveryReady: boolean;
+  incrementQuantity: (id: number) => void;
+  decrementQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  setDeliveryStatus: (status: boolean) => void;
 }
 
-const CartView = ({ products, removeFromCart }: IProps) => {
-  console.log(products, 'products');
-  const [productsData, setProductsData] = useState<IProductsData[]>(products);
-  console.log(productsData, 'CartView productsData');
-
-  useEffect(() => {
-    setProductsData(products);
-  }, [products]);
+const CartView = ({
+  products,
+  totalPrice,
+  isDeliveryReady,
+  incrementQuantity,
+  decrementQuantity,
+  removeFromCart,
+  setDeliveryStatus,
+}: IProps) => {
+  const handleOrderStatusToggle = (): void => {
+    setDeliveryStatus(!isDeliveryReady);
+  };
 
   return (
     <>
-      {productsData.length ? (
+      <h1 className={s.title}>In cart</h1>
+      {products.length ? (
         <>
-          <h1>In cart</h1>
-          {productsData.map(({ id, name, price }) => {
-            return (
-              <div key={id}>
-                <h5>{name}</h5>
-                <h6>{price}</h6>
-                {/* <button type="button" onClick={() => handleAddProduct(id)}>
-                  Add to cart
-                </button> */}
+          <div className={s.wrapper}>
+            {products.map(({ id, name, price, quantity }) => {
+              return (
+                <div key={id} className={s.card}>
+                  <h4>{name}</h4>
+                  <h5>price: {price}$</h5>
+                  <h6>quantity: {quantity}</h6>
 
-                {/* <button type="button" onClick={handleAddProduct}>
-                Remove from cart
-              </button> */}
-              </div>
-            );
-          })}
+                  <Controls
+                    onIncrement={() => incrementQuantity(id)}
+                    onDecrement={() => decrementQuantity(id)}
+                  />
+
+                  <button type="button" onClick={() => removeFromCart(id)}>
+                    Remove from cart
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+          {!!totalPrice && (
+            <div>
+              <h3 className={s.totalPrice}>Total price: {totalPrice}</h3>
+              {!isDeliveryReady && (
+                <button type="button" onClick={handleOrderStatusToggle}>
+                  Make order
+                </button>
+              )}
+            </div>
+          )}
         </>
       ) : (
-        <h1>Nothing in cart</h1>
+        <h2>Nothing in cart</h2>
       )}
     </>
   );
