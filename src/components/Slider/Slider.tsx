7a5ea259ddcca0d8c6ab8b8a661704f93a30c0d3
Roman <77742141+rumpear@users.noreply.github.com>
2022-10-25 00:22:33 +0300

    Slider refactoring

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,56 +1,31 @@
 import { useCallback, useEffect, useState } from 'react';
-import classNames from 'classnames/bind';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { Cards } from './Cards';
 import { ButtonIcon } from '../ui/ButtonIcon';
 import { PHOTOS_ARR } from '../../data/';
-import { ICard } from './types';
+import { ICard, TCardAnimationType } from './types';
 import s from './Slider.module.css';
 
-const slides = ['first', 'second', 'third'];
-
 const Slider = () => {
-  const cx = classNames.bind(s);
-
   const [cards, setCards] = useState<ICard[]>([]);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [currentLevel, setCurrentLevel] = useState(0);
-  const [isCardAnimated, setIsCardAnimated] = useState(false);
+  const [cardAnimationType, setCardAnimationType] =
+    useState<TCardAnimationType>('');
   const totalSlidesIdx = cards.length - 1;
 
   const showPrevCard = useCallback(() => {
     setCurrentSlide(prev => {
-      if (prev <= 0) {
-        return totalSlidesIdx;
-      }
-      return prev - 1;
+      return prev <= 0 ? totalSlidesIdx : prev - 1;
     });
+    setCardAnimationType('back');
   }, [totalSlidesIdx]);
 
   const showNextCard = useCallback(() => {
     setCurrentSlide(prev => {
-      if (prev >= totalSlidesIdx) {
-        return 0;
-      }
-      return prev + 1;
+      return prev >= totalSlidesIdx ? 0 : prev + 1;
     });
+    setCardAnimationType('forward');
   }, [totalSlidesIdx]);
-
-  // const showPrevCard = useCallback(() => {
-  //   const deleteCount = cards.length;
-  //   const startIdx = deleteCount - 1;
-  //   const splicedCard = cards.splice(startIdx, deleteCount);
-  //   setCards([...splicedCard, ...cards]);
-  //   setIsCardAnimated(true);
-  // }, [cards]);
-
-  // const showNextCard = useCallback(() => {
-  //   const startIdx = 0;
-  //   const deleteCount = startIdx + 1;
-  //   const splicedCard = cards.splice(startIdx, deleteCount);
-  //   setCards([...cards, ...splicedCard]);
-  //   setIsCardAnimated(true);
-  // }, [cards]);
 
   const handleArrowBtnsClick = useCallback(
     (e: KeyboardEvent) => {
@@ -83,48 +58,11 @@ const Slider = () => {
         <AiOutlineArrowLeft />
       </ButtonIcon>
 
-      {/* {cards.length && (
-        <Cards cardsData={cards} isCardAnimated={isCardAnimated} />
-      )} */}
-
-      {cards.length && (
-        <div className={s.cardWrapper}>
-          {slides.map((slide, idx, arr) => {
-            const currIndex = (idx + currentSlide) % arr.length;
-            // console.log(idx, 'idx');
-            // console.log(currIndex, 'currIndex');
-            // console.log(arr.length, 'arr.length');
-
-            // if (idx === currentSlide) {
-            //   console.log(idx, 'idx');
-            // }
-
-            // console.log(currentSlide, 'currentSlide');
-            return (
-              <div
-                key={slide}
-                className={cx([
-                  // 'Card',
-                  `Card--${arr[currIndex]}`,
-                  // { isActive: idx === currentSlide },
-                ])}
-              >
-                <h1 className={s.cardTitle}>{cards[currIndex].title}</h1>
-                <p className={s.cardAnnotation}>
-                  {cards[currIndex].annotation}
-                </p>
-                <div className={s.cardThumb}>
-                  <img
-                    className={s.cardImg}
-                    src={cards[currIndex].image}
-                    alt={cards[currIndex].title}
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+      <Cards
+        cardsData={cards}
+        currentSlide={currentSlide}
+        cardAnimationType={cardAnimationType}
+      />
 
       <ButtonIcon onClick={showNextCard}>
         <AiOutlineArrowRight />
--- src/components/Slider/types.ts
+++ src/components/Slider/types.ts
@@ -4,3 +4,5 @@ export interface ICard {
   annotation: string;
   id: number;
 }
+
+export type TCardAnimationType = 'forward' | 'back' | '';
